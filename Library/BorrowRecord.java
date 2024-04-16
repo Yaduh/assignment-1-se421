fd7469e1c19df8e69b19fdbcd4fab44b890ad0e3
HemaShaswar <96527414+HemaShaswar@users.noreply.github.com>
2024-04-16 19:57:17 +0300

    Update BorrowRecord.java

--- Library/BorrowRecord.java
+++ Library/BorrowRecord.java
@@ -1,8 +1,19 @@
-package Library;
+import java.util.Date;
 
 public class BorrowRecord {
     private Borrower borrower;
     private Item item;
+    private Date BorrowDate;
+
+    public BorrowRecord(Borrower borrower, Item item){
+        setBorrowDate();
+        setBorrower(borrower);
+        setItem(item);
+    }
+
+    public void setBorrowDate() {
+        this.BorrowDate = new Date();
+    }
 
     public void setBorrower(Borrower borrower) {
         this.borrower = borrower;
@@ -10,6 +21,10 @@ public class BorrowRecord {
 
     public void setItem(Item item) {
         this.item = item;
+    }
+
+    public Date getBorrowDate() {
+        return this.BorrowDate;
     }
 
     public Borrower getBorrower() {
