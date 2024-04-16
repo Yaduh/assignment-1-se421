b55839106a7c9fa5315cfb4005f133a89b9dd865
HemaShaswar <96527414+HemaShaswar@users.noreply.github.com>
2024-04-16 19:56:13 +0300

    Update LibraryManager.java

--- Library/LibraryManager.java
+++ Library/LibraryManager.java
@@ -1,82 +1,37 @@
-package Library;
-
 import java.util.HashMap;
-import java.util.Date;
-public class LibraryManager {
-    private Item item;
-    private Borrower borrower;
-    private Date startDate;
-    private Date dueDate;
 
-    public HashMap<Borrower, Item> borrowRecord = new HashMap();
-    
-    // private final List<LendRecord> lendRecords = new ArrayList<>();
+public class LibrarySystem {
+    private HashMap<Integer,BorrowRecord> records = new HashMap<>();
 
-    // public void lendItem(LibraryItem item, int memberId, LocalDate startDate) {
-    //     if (lendRecords.size() >= 5) {
-    //         System.out.println("Cannot lend more than 5 items.");
-    //         return;
-    //     }
+    public BorrowRecord getRecord(int ItemID){
+        return this.records.get(ItemID);
+    }
 
-    //     lendRecords.add(new LendRecord(item, memberId, startDate));
-    //     System.out.println("Item lent successfully.");
-    //     printLendRecords();
-    // }
-    // public void returnItem(LibraryItem item, int memberId, LocalDate returnDate) {
-    //     LendRecord recordToRemove = null;
-    //     for (LendRecord record : lendRecords) {
-    //         if (record.getItem().getLibraryId() == item.getLibraryId() && record.getMemberId() == memberId) {
-    //             recordToRemove = record;
-    //             break;
-    //         }
-    //     }
+    public void addRecord(Borrower borrower,Item item){
+        BorrowRecord newRecord = new BorrowRecord(borrower,item);
+        this.records.put(item.getId(), newRecord);
+        displayBorrowRecords();
+    }
     
-    //     if (recordToRemove != null) {
-    //         lendRecords.remove(recordToRemove);
-    //         System.out.println("Item returned successfully.");
-    //         printLendRecords();
-    //     } else {
-    //         System.out.println("No matching lend record found.");
-    //     }
-    // }
-    
-    // private void printLendRecords() {
-    //     System.out.println("Lend Records:");
-    //     System.out.println("--------------------------------------------------------");
-    //     System.out.println("Library ID | Title            | Due Date    | Fine");
-    //     System.out.println("--------------------------------------------------------");
-    //     for (LendRecord record : lendRecords) {
-    //         LocalDate currentDate = LocalDate.now();
-    //         long daysLate = Math.max(0, currentDate.compareTo(record.getDueDate()));
-    //         double fine = calculateFine(record.getItem(), daysLate);
-    //         System.out.printf("%-10s | %-16s | %-12s | $%.2f%n",
-    //                 record.getItem().getLibraryId(),
-    //                 record.getItem().getTitle(),
-    //                 record.getDueDate(),
-    //                 fine);
-    //     }
-    //     System.out.println("--------------------------------------------------------");
-    // }
-    
-    // private double calculateFine(LibraryItem item, long daysLate) {
-    //     double finePerDay;
-    //     switch (item.getClass().getSimpleName()) {
-    //         case "Book":
-    //             finePerDay = 3.0;
-    //             break;
-    //         case "Journal":
-    //             finePerDay = 3.0;
-    //             break;
-    //         case "Video":
-    //             finePerDay = 1.0;
-    //             break;
-    //         case "TechnicalPaper":
-    //             finePerDay = 2.0;
-    //             break;
-    //         default:
-    //             finePerDay = 0.0;
-    //             break;
-    //     }
-    //     return finePerDay * daysLate;
-    // }
+    public void deleteBorrowRecord(int itemID){
+        this.records.remove(itemID);
+        displayBorrowRecords();
+    }
+
+    private void displayBorrowRecords(){
+        System.out.println();
+        records.forEach((key, value) -> {
+            Borrower b = value.getBorrower();
+            Item i = value.getItem();
+            
+            System.out.println(b.getBorrowerType() 
+            + " " + b.getId()  
+            + " borrowed " + i.getType() 
+            + " " + i.getId() 
+            + " at " + value.getBorrowDate());
+            
+        });
+        System.out.println("____________");
+
+    }
 }
