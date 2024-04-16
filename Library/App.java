10685ba20f72ebc4786a82b574bce6ca471f445c
HemaShaswar <96527414+HemaShaswar@users.noreply.github.com>
2024-04-16 19:54:38 +0300

    Update App.java

--- Library/App.java
+++ Library/App.java
@@ -1,11 +1,18 @@
-package Library;
 
 public class App {
     public static void main(String[] args) {
-        LibraryManager record = new LibraryManager();
-        Item i1 = new Book(0, null, null);
         Borrower b1 = new Faculty(10);
-        record.borrowRecord.put(b1, i1);
-        System.out.println(record.borrowRecord);
+        Borrower b2 = new Student(20);
+
+        Item i1 = new Book(10, "Book 1", "test book");
+        Item i2 = new Video(20, "Video 1", "test video");
+
+        LibrarySystem librarySystem = new LibrarySystem();
+        librarySystem.addRecord(b1, i1);
+        librarySystem.addRecord(b2, i2);
+
+        librarySystem.deleteBorrowRecord(10);
+
     }
+    
 }
