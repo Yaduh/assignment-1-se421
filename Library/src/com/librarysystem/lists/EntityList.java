8cf0a68f97fe9ff8c62822a9b00a02293dda55bb
Yaduh <137622535+Yaduh@users.noreply.github.com>
2024-05-21 02:00:45 +0300

    naming

--- Library/src/com/librarysystem/lists/EntityList.java
+++ Library/src/com/librarysystem/lists/EntityList.java
@@ -2,9 +2,9 @@ package com.librarysystem.lists;
 
 import java.util.List;
 
-public interface EntityList<T> {
-    void addEntity(T entity);
-    void removeEntity(T entity);
-    T getEntityById(int id);
-    List<T> getEntityList();
+public interface EntityList<Entity> {
+    void addEntity(Entity entity);
+    void removeEntity(Entity entity);
+    Entity getEntityById(int id);
+    List<Entity> getEntityList();
 }
